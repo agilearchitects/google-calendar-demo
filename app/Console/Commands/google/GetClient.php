5265cab6fc166271dd35da057f10f30e2ad24257
hannes <hannes@agilearchitects.se>
2020-09-07 20:41:45 +0200

    Removes dp injection of google client in getClient command since it's not recommended to dp inject in artisan commands: https://laravel.com/docs/5.2/artisan#writing-commands

--- app/Console/Commands/google/GetClient.php
+++ app/Console/Commands/google/GetClient.php
@@ -1,6 +1,8 @@
 <?php namespace App\Console\Commands\google;
 
 use Illuminate\Console\Command;
+use Google_Client;
+use App\FS\FileSystem;
 
 class GetClient extends Command
 {
@@ -19,24 +21,19 @@ class GetClient extends Command
     protected $description = 'Get Google Client credentials';
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct(\App\Google\Client $client)
-    {
-        $this->client = $client;
-        parent::__construct();
-    }
-
-    /**
      * Execute the console command.
      *
      * @return mixed
      */
     public function handle()
-    {
-        $this->client->auth(function(string $authUrl) {
+    {   
+        /* Create google client. Was uable to use DP injector
+        in constructor since each registered command is created
+        on application initiation and therefore run the constructor
+        of  \App\Google\Client::class. Which will fail if json-files
+        are missing. Which they do when running automated testing*/
+        $client = new \App\Google\Client(new Google_Client(), new FileSystem(), "./credentials.json", "./token.json");
+        $client->auth(function(string $authUrl) {
             $this->info("Go to url \"{$authUrl}\" for authentication");
             $code = $this->ask("Paste auth code here:");
             return $code;
